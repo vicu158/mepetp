f80d901f4b1184991cf02bdc3feaf887cd2125a6
vicu158 <v_evlashev@mail.ru>
2024-07-21 16:17:03 +0300

    1.14

--- store-server/store/store/settings.py
+++ store-server/store/store/settings.py
@@ -79,6 +79,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
