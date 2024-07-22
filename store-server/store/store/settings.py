2151edc67115a2d641c617b63506bf1007fd09fc
vicu158 <v_evlashev@mail.ru>
2024-07-22 23:28:38 +0300

    4.6

--- store-server/store/store/settings.py
+++ store-server/store/store/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -88,3 +89,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
